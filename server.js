3d62c6fe87e44f66556af4e7f761317bbb656152
mcnairjm19 <mcnairjm1993@gmail.com>
2022-02-02 12:45:30 -0500

    working routes

--- server.js
+++ server.js
@@ -5,7 +5,24 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// middle ware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Route to index
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Route to notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
 
 app.listen(3001, () => {
     console.log(`Server on port 3001`);
 })
+
+
+
