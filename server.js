1417be3e2ee6669c61365a137b8888595ab91a3d
mcnairjm19 <mcnairjm1993@gmail.com>
2022-02-06 16:43:17 -0500

    added post and delete functions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const note = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,8 +21,76 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
-app.listen(3001, () => {
-    console.log(`Server on port 3001`);
+app.get('api/notes', (req, res) => {
+    res.json(note);
+});
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
+        const db = JSON.parse(data);
+        const newDB = [];
+
+        db.push(req.body);
+
+        for (let i = 0; i < db.length; i++)
+        {
+            const newNote = {
+                title: db[i].title,
+                text: db[i].text,
+                id: i
+            };
+
+            newDB.push(newNote);
+        }
+
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
+            if (err) throw err;
+            res.json(req.body);
+        });
+    });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
+        const db = JSON.parse(data);
+        const newDB = [];
+
+        for(let i = 0; i < db.length; i++)
+        {
+            if (i !== id)
+            {
+                const newNote = {
+                    title: db[i].title,
+                    text: db[i].text,
+                    id: newDB.length
+                };
+
+                newDB.push(newNote);
+            }
+        }
+
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
+            if (err) throw err;
+            res.json(req.body);
+        });
+    });
+});
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`);
 })
 
 
